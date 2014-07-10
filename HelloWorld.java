d0193099c07ecbf6752b8f89c94319c50a76fdfb
xilaoda <xilaoda@zhongnanhai.com>
2014-07-10 16:55:17 +0800

    1.0版本去打上TAG

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ class  HelloWorld
 		System.out.println("Hello World!");
 		System.out.println("lyd is coding");
 		System.out.println("qiubenchao is coding");
+		System.out.println("1.0 end make a tag");
 	}
 }
